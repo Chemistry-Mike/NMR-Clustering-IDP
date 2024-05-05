0ce64bdf86b13525ae133f6852a85ae5dca343b4
Chemistry-Mike <125370921+Chemistry-Mike@users.noreply.github.com>
2024-05-05 20:25:35 +0200

    Create generate_DR_latent_space.py

---
+++ generate_DR_latent_space.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.manifold import TSNE
+from deeptime.decomposition import TICA
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import pickle
+import sys
+import math
+import mdtraj as md
+
