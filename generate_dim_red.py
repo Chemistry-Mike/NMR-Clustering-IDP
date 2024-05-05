b2138ac5c72a7ca393412ab834326d730f125481
Chemistry-Mike <125370921+Chemistry-Mike@users.noreply.github.com>
2024-05-05 21:02:27 +0200

    Update generate_dim_red.py

--- generate_dim_red.py
+++ generate_dim_red.py
@@ -9,48 +9,48 @@ import sys
 import math
 import mdtraj as md
 
-### 
-HOME="/mnt/proj1/open-27-2/bakkerm/"
+### Specify the HOME directory 
+HOME="path/to/dir"
 
-### 
+### Specifies the name of the system being investigated
 protein="TEST"
 
-### 
+### Specify the location of the features pickle (input)
 INFO = pickle.load(open(HOME+"6-Conda/1-PICKLES/INFO/"+protein+"-INFO.pkl","rb"))
-### 
+### Specify the location of the pickle file (output) 
 PKL = HOME+"6-Conda/1-PICKLES/DR/"+protein+"-DimRed.pkl"
 
-### 
+### Generate a DataFrame of the alpha carbons from the trajectory
 PDB = HOME+"0-Trajectories/"+protein+"-first.pdb"
 traj=md.load(PDB,top=PDB)
 atoms, bonds = traj.topology.to_dataframe()
 
-### 
+### Generate lists of all the oxygen, hydrogen, and nitrogen atoms
 o_atoms = atoms[atoms.element=="O"].index
 h_atoms = atoms[atoms.element=="H"].index
 n_atoms	= atoms[atoms.element=="N"].index
 ho_atoms = atoms[(atoms.element=="O")|(atoms.element=="H")].index
 
-### 
+### Creates a function to do a linear PCA dimensionality reduction on the input features
 def runPCA(data,ndim=2):
-    pca = PCA(n_components=ndim).fit_transform(data[::dt])
+    pca = PCA(n_components=ndim).fit_transform(data)
     pca_data=pca[:, :2].T
     return pca_data
 
-### 
+### Creates a function to do a linear tICA dimensionality reduction on the input features
 def runTICA(features, dimensions=2, lag=20):
     estimator = TICA(dim=dimensions, lagtime=lag).fit(features)
     tica_data = estimator.fetch_model().transform(features[::dt]).T
     return tica_data
 
-### 
+### Creates a function to do a nonlinear tICA dimensionality reduction on the input features
 def runtSNE(data,perp=40,dim=2):
     print("Running tSNE...")
     tsne = TSNE(n_components=dim, random_state=42, perplexity=perp)
     tsne_data = tsne.fit_transform(data[::dt])
     return tsne_data
 
-### 
+### Introduces a standard scalar to each of the input features
 scaler = StandardScaler()
 phipsi=scaler.fit_transform(np.hstack([INFO["phi"],INFO["psi"]]))
 distang=scaler.fit_transform(np.hstack([INFO["dist"],INFO["ang"]]))
@@ -63,7 +63,7 @@ all=scaler.fit_transform(np.hstack([phipsi,distang,sasa]))
 all_o=scaler.fit_transform(np.hstack([phipsi,distang,sasa_o]))
 all_ho=scaler.fit_transform(np.hstack([phipsi,distang,sasa_ho]))
 
-### 
+### Generates the tICA latent spaces
 info={}
 info["tica"]={}
 info["tica"]["phipsi"]=runTICA(phipsi)
@@ -77,7 +77,7 @@ info["tica"]["all"]=runTICA(all)
 info["tica"]["all_o"]=runTICA(all_o)
 info["tica"]["all_ho"]=runTICA(all_ho)
 
-### 
+### Generates the PCA latent spaces
 info["pca"]={}
 info["pca"]["phipsi"]=runPCA(phipsi)
 info["pca"]["distang"]=runPCA(distang)
@@ -90,7 +90,7 @@ info["pca"]["all"]=runPCA(all)
 info["pca"]["all_o"]=runPCA(all_o)
 info["pca"]["all_ho"]=runPCA(all_ho)
 
-### 
+### Generate the tSNE latent spaces while scanning through a list of perplexities. This may need to be adjusted after observing the resultant DRs.
 info["tsne"]={}
 for perp in [10,20,30,40,50,60,70,80,90,100,150,200]:
     info["tsne"][perp]={}
@@ -105,5 +105,5 @@ for perp in [10,20,30,40,50,60,70,80,90,100,150,200]:
     info["tsne"][perp]["all_o"]=runtSNE(all_o,perp)
     info["tsne"][perp]["all_ho"]=runtSNE(all_ho,perp)
 
-### 
+### Dump the DICT into a pickle file for later analysis.
 pickle.dump(info,open(PKL,'wb'))
